a41145e65bc0d8ceae822479bd03eb89cf8fa47c
Roland Broekema <roland.broekema@qnh.nl>
2019-01-02 11:00:43 +0100

    no need for the session userid logic anymore. now handled by the socket connection

--- server.js
+++ server.js
@@ -3,7 +3,6 @@
 var express = require('express');
 var compression = require('compression');
 var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
 var methodOverride = require('method-override');
 var errorHandler = require('errorhandler');
 var app = express();
@@ -14,25 +13,6 @@ var schedule = require('node-schedule');
 schedule.scheduleJob('0 0 * * *', function() {
   console.log('some update for twitter or other feed here');
 });
-
-function uuidv4() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
-    return v.toString(16);
-  });
-}
-
-function checkSession(req, res) {
-  if(!req.cookies.SESSIONID) {
-    res.cookie('SESSIONID', uuidv4(), { maxAge: 30 * 60 * 1000, httpOnly: true });
-  } else {
-    res.cookie('SESSIONID', req.cookies.SESSIONID, { maxAge: 30 * 60 * 1000, httpOnly: true });
-  }
-  if(!req.cookies.USERID) {
-    res.cookie('USERID', uuidv4(), { maxAge: 900 * 24 * 60 * 60 * 1000, httpOnly: true });
-  }
-}
-
 
 // enable express strict routing, see http://expressjs.com/api.html#app-settings
 // for more info
@@ -47,7 +27,6 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(bodyParser.json());
-app.use(cookieParser());
 app.set('views', path.join(__dirname, 'src', 'tmpl'));
 app.set('view engine', 'pug');
 
@@ -73,7 +52,6 @@ console.log('Starting a server on port: ' + port);
  * express app router
  */
 app.get('/', function (req, res, next) {
-  checkSession(req, res);
   var filePath = 'build' + req.url + '.html';
   if (req.url === '/') {
     filePath = 'build/index.html';
@@ -90,7 +68,6 @@ app.get('/', function (req, res, next) {
 
 // api routes
 app.get('/api*', function (req, res, next) {
-  checkSession(req, res);
   req.url = req.url.toLowerCase();
   var filePath = 'build' + req.url + '.html';
 
@@ -111,7 +88,6 @@ app.get('/api*', function (req, res, next) {
 
 // news route
 app.get('/blog*', function (req, res, next) {
-  checkSession(req, res);
   var cleanUrl = req.url.split('?')[0];
   var filePath = 'build' + cleanUrl + '.html';
 
@@ -147,7 +123,6 @@ app.get('*', function (req, res, next) {
   }
 
   fs.exists(filePath, function (exists) {
-    checkSession(req, res);
     if (exists) {
       res.sendFile(filePath, {root: __dirname});
     } else {
