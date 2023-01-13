7b7f4077d21a99baaa48ea38155f57ca3c5bdad2
Roland Broekema <broekema41@gmail.com>
2023-01-13 10:29:35 +0100

    adding samesite property to cookies to not show warning in newer browsers

--- assets/js/utils/cookie.js
+++ assets/js/utils/cookie.js
@@ -18,6 +18,6 @@ export default {
     var d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
     var expires = "expires=" + d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/;SameSite=strict";
   }
 }
